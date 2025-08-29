d5f97fb96a4f84ec574ff9160707bd89370529d9
Jan Diehm <jadiehm@gmail.com>
2025-08-29 16:03:43 -0500

    update github pages

--- docs/_app/immutable/chunks/DadvkfaR.js
+++
@@ -1,1 +0,0 @@
-import{l as t,u as e,k as c,m as u}from"./CCT82uQH.js";function s(o){c===null&&t(),e(()=>{const n=u(o);if(typeof n=="function")return n})}export{s as o};
--- docs/_app/immutable/entry/start.mv-uiiN6.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BacB-T7n.js";export{o as load_css,r as start};
